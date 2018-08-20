b05be78273382c5cdac6121244a855cf238bdc26
Gregory Tunell <gregtunell@gmail.com>
2018-08-20 15:40:26 -0400

    Appveyor needs compiler

--- deps/build.jl
+++ deps/build.jl
@@ -5,7 +5,8 @@ end
 Pkg.add("MAT")
 
 if is_windows()
-    run(`cmake -DCMAKE_SH="CMAKE_SH-NOTFOUND" -G "MinGW Makefiles" .`)
+    #run(`cmake -DCMAKE_SH="CMAKE_SH-NOTFOUND" -G "MinGW Makefiles" .`)
+    run(`cmake -DCMAKE_IGNORE_PATH="C:/Program Files/Git/usr/bin" -G "MinGW Makefiles" .`)
 else
     run(`cmake .`)
 end
