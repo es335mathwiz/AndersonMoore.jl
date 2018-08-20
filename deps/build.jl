d6317f307d971f366776b1d8649dccd6ae199de0
Gregory Tunell <gregtunell@gmail.com>
2018-08-20 13:28:32 -0400

    build.jl fixes

--- deps/build.jl
+++ deps/build.jl
@@ -1,12 +1,11 @@
 if is_apple()
-#    run(`brew tap homebrew/science`)
     run(`brew install hdf5`)
     Pkg.add("HDF5")
 end
 Pkg.add("MAT")
 
 if is_windows()
-    run(`cmake -G "MinGW Makefiles" .`)
+    run(`cmake -DCMAKE_SH="CMAKE_SH-NOTFOUND" -G "MinGW Makefiles" .`)
 else
     run(`cmake .`)
 end
