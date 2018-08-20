d1c72df3203c89574f3e2667b904b8874c9790e0
Gregory Tunell <gregtunell@gmail.com>
2018-08-20 11:33:04 -0400

    testing build

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,12 @@
+if is_linux() 
+   Pkg.add("MAT")
+elseif is_apple()
+   run(`brew tap homebrew/science`)
+   run(`brew install hdf5`)
+   Pkg.add("HDF5")
+   Pkg.add("MAT")';
+end
+
 if is_windows()
    run(`cmake -G "MinGW Makefiles" .`)
 else
