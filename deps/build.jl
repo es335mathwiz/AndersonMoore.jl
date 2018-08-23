935347f46f4f8faa0435fff5dea56fa31c9809ed
Gregory Tunell <gregtunell@gmail.com>
2018-08-23 11:55:17 -0400

    prepull :

--- deps/build.jl
+++ deps/build.jl
@@ -4,14 +4,9 @@ if is_apple()
 end
 Pkg.add("MAT")
 
-#if is_windows()
-#    run(`cmake -DCMAKE_IGNORE_PATH="C:/Program Files/Git/usr/bin" -G "MinGW Makefiles" .`)
-#else
-#    run(`cmake .`)
-#end
-#run(`make`)
-
-if is_apple() || is_linux()
+if is_windows()
+    run(`cmake -DCMAKE_IGNORE_PATH="C:/Program Files/Git/usr/bin" -G "MinGW Makefiles" .`)
+elseif is_apple() || is_linux()
    run(`cmake .`)
    run(`make`)
 end
